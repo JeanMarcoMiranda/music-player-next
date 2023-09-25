bbff1bab5adb17bf4120b7db509f905da2226f20
BrucceVT <bvillena2000@gmail.com>
2023-09-24 21:49:10 -0500

    adding providers, authentication modal and functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import Sidebar from "@/components/sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/supabase-provider";
+import UserProvider from "@/providers/user-provider";
+import ModalProvider from "@/providers/modal-provider";
+import ToasterProvider from "@/providers/toaster-provider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -17,10 +21,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={font.className + " bg-gray-950 text-white"} suppressHydrationWarning>
-        <Sidebar>
-          {children}
-        </Sidebar>
+      <body
+        className={font.className + " bg-gray-950 text-white"}
+        suppressHydrationWarning
+      >
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
         {/* <main className="container m-auto h-[calc(100vh-4rem)] flex items-center justify-center">
           {children}
         </main> */}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,17 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
 import Button from "./button";
 
 interface HeaderProps {
@@ -12,11 +20,22 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, classname }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
 
-  const handleLogout = () => {
-    // Handle logout
-  };
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    router.refresh();
+
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success('Logged out!')
+    }
+  }
 
   return (
     <div
@@ -44,30 +63,51 @@ const Header: React.FC<HeaderProps> = ({ children, classname }) => {
 
         {/* Mobil */}
         <div className="flex md:hidden gap-x-2 item-center">
-          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition" title="Home" aria-label="Home">
             <HiHome className="text-black" size={20} />
           </button>
-          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition" title="Search" aria-label="Search">
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
 
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div>
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
               <Button
-                onClick={() => {}}
-                className="bg-transparent text-neutral-300 font-medium"
+                onClick={() => router.push("/account")}
+                className="bg-white"
               >
-                Sign up
+                <FaUserAlt />
               </Button>
             </div>
-            <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">
-                Log in
-              </Button>
-            </div>
-          </>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
+                    bg-transparent 
+                    text-neutral-300 
+                    font-medium
+                  "
+                >
+                  Sign up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-white px-6 py-2"
+                >
+                  Log in
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
 
